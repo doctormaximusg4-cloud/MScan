5991c34b9896bb043fd4f3e264b59b23b294419c
doctormaximusg4-cloud <doctormaximusg4@gmail.com>
2026-08-28 10:51:47 +0300

    Delete mscan-camera.js

--- mscan-camera.js
+++
@@ -1,5 +0,0 @@
-var exec=require('cordova/exec');
-module.exports={
- start:function(success,error){exec(success,error,'MScanCamera','start',[]);},
- stop:function(success,error){exec(success,error,'MScanCamera','stop',[]);}
-};
